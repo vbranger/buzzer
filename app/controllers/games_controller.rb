e3721cbfca29fcef614ed378a5e76d2cf45f1eab
Victor Branger <victor.branger@gmail.com>
2020-12-09 22:59:54 +0100

    create game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,4 +3,19 @@ class GamesController < ApplicationController
     @game = Game.find(params[:id])
     @buzz = Buzz.new
   end
+
+  def create
+    @game = Game.new
+    @game.user_id = current_user.id
+    @game.number = @game.id
+    @game.save
+
+    redirect_to game_path(@game)
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:number, :user_id)
+  end
 end
