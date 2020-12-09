b4011f7d702e68d9718003adbe090d14cece7e84
Victor Branger <victor.branger@gmail.com>
2020-12-09 21:55:29 +0100

    added owner for games

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_170628) do
+ActiveRecord::Schema.define(version: 2020_12_09_205221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2020_12_09_170628) do
     t.integer "number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_games_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,4 +48,5 @@ ActiveRecord::Schema.define(version: 2020_12_09_170628) do
 
   add_foreign_key "buzzs", "games"
   add_foreign_key "buzzs", "users"
+  add_foreign_key "games", "users"
 end
