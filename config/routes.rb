0f0df054466774598b96ea1ea02216bb40a05ba8
Victor Branger <victor.branger@gmail.com>
2020-12-15 10:23:16 +0100

    liste of games

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :games, only: [:show, :create] do
+  resources :games, only: [:show, :create, :index] do
     resources :buzzs, only: :create
   end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
