d124242c476356d4744428adcf4696b7f46720d5
Victor Branger <victor.branger@gmail.com>
2021-01-06 17:34:59 +0100

    added button and route for find game

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   end
   get 'clearbuzzs', to: 'buzzs#clearbuzzs'
   get 'create_new_game', to: 'games#create_new_game'
+  get 'find_game', to: 'games#find'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
