a8626fdea7920417af0e645bfbddb4401c088b94
Victor Branger <victor.branger@gmail.com>
2021-09-22 18:14:52 +0200

    fix prod

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  mount ActionCable.server => '/cable'
+
   resources :games, only: [:show, :create, :index, :destroy] do
     resources :buzzs, only: :create
   end
